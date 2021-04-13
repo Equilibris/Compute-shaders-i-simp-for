bb854c31a65d50d8645b395b76ad11177d090635
William Sørensen <william@resight.no>
2021-04-13 13:33:51 +0200

    boid work

--- Assets/Scenes/boids/Boids.cs
+++ Assets/Scenes/boids/Boids.cs
@@ -4,25 +4,44 @@ using UnityEngine;
 
 public class Boids : BaseRenderer
 {
+	private ComputeBuffer mainBuffer;
+
 	private int KERNEL_ID_Init;
+	private int KERNEL_ID_Update;
 	private int KERNEL_ID_Render;
 
-	public int length = 100;
+	public int count = 100;
 	private int stride = 4 * 4;
 
 	private Vector2 ThreadBlockSize = new Vector2(8, 8);
+	private float InitBlockLength = 64;
 
 	private void Awake()
 	{
 		KERNEL_ID_Render = MainShader.FindKernel("Render");
+		KERNEL_ID_Init = MainShader.FindKernel("Init");
+		KERNEL_ID_Update = MainShader.FindKernel("Update");
 
-        // TODO: ADD BUFFER
+		InitComputeBuffer();
+	}
+	private void InitComputeBuffer()
+	{
+		mainBuffer = new ComputeBuffer(count, stride);
+
+		SetShaderParams();
+
+		MainShader.SetBuffer(KERNEL_ID_Init, "Boids", mainBuffer);
+		MainShader.Dispatch(KERNEL_ID_Init, Mathf.CeilToInt(count / InitBlockLength), 1, 1);
 	}
 
-
+	private void OnDestroy()
+	{
+		if (mainBuffer != null) mainBuffer.Release();
+		if (Result != null) Result.Release();
+	}
 	public override void SetShaderParams()
 	{
-
+		MainShader.SetInt("count", count);
 	}
 	public override void Render(RenderTexture destination)
 	{
